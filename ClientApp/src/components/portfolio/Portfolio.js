e0bb2f5b0068f3b60ec2aa212415d2725ab4a038
Andre Burke <aburkex24@gmail.com>
2018-11-22 19:39:05 -0700

    added Portfolio react class with some child classes

---
+++ ClientApp/src/components/portfolio/Portfolio.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './Portfolio.css';
+
+export class Portfolio extends Component {
+    displayName = Portfolio.name;
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+        return (
+            <h1>Hello World!</h1>
+        );
+    }
+}
+
+class Title extends Component {
+    constructor(props) {
+        super(props);
+    }
+}
+
+class Bio extends Component {
+    constructor(props) {
+        super(props);
+    }
+}
+
+class Skill extends Component {
+    constructor(props) {
+        super(props);
+    }
+}
+
+class Resume extends Component {
+    constructor(props) {
+        super(props);
+    }
+}
+
+class Project extends Component {
+    constructor(props) {
+        super(props);
+    }
+}
+
+class Contact extends Component {
+    constructor(props) {
+        super(props);
+    }
+}
