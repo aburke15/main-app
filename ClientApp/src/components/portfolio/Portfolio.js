e839b9b60ba4e0c855cca4e77840e62d7bfe140f
Andre Burke <aburkex24@gmail.com>
2018-11-23 00:53:40 -0700

    added a few more components for the portfolio

--- ClientApp/src/components/portfolio/Portfolio.js
+++ ClientApp/src/components/portfolio/Portfolio.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Grid, Col, Row } from 'react-bootstrap';
 import './Portfolio.css';
+import { BioImg } from '../img/portfolio/prof_pic_2018.jpeg';
 
 export class Portfolio extends Component {
     displayName = Portfolio.name;
@@ -12,6 +13,7 @@ export class Portfolio extends Component {
         return (
             <div className="portfolio-font">
                 <Title />
+                <Bio />
             </div>
         );
     }
@@ -24,12 +26,12 @@ class Title extends Component {
 
     render() {
         return (
-            <Grid className="title-section parallax">
-                <Grid bsClass="container">
+            <section className="title-section parallax">
+                <div bsClass="container">
                     <h1 className="text-center title title-color">Andre Burke</h1>
                     <h2 className="text-center sub-title title-color">Software Engineer</h2>
-                </Grid>
-            </Grid>
+                </div>
+            </section>
         );
     }
 }
@@ -37,6 +39,42 @@ class Title extends Component {
 class Bio extends Component {
     constructor(props) {
         super(props);
+    }
+
+    render() {
+        return (
+            <section>
+                <div className="container">
+                    <Row>
+                        <h2 class="text-center">About Me</h2>
+                        <p class="b-underline"></p>
+                    </Row>
+                    <Row className="global-margin">
+                        <img class="img-responsive bio-pic" src="../img/portfolio/prof_pic_2018.jpeg" />
+                    </Row>
+                    <Row>
+                        <p class="text-center">Whats sparked my interest in coding was my love for technology. For several
+                        years
+                        now
+                        I've been an
+                        actively
+                        involved tech enthusiast. it started with smartphones. My very first smartphone was the iPhone 3GS,
+                        it was my first step into the newly arisen smartphone revolution. Currently I am interested in both
+                        Android and
+                        Apple devices and will interchange between platforms.
+                        Not long after I began to develop love for all sorts of
+                        technology and gadgets. After so long I decided that using and admiring tech wasn't enough for me
+                        so I turned to something even better, programming. It became obvious to me that I wanted to develop
+                        software for the devices I was so invested in. My first programming language was C#, as I was
+                        learning
+                        to develop in C# it felt like a whole new world of possibilities and that is what I absolutely
+                        love
+                        about programming, you can do most anything with it.
+                        </p>
+                    </Row>
+                </div>
+            </section>
+        );
     }
 }
 
